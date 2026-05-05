1dd7df2c4b879cce594a993d8371e7743e6cc669
sandy424 <sandy424@naver.com>
2026-05-05 16:42:27 +0900

    fix: 메인페이지 파일 업로드 폼 만들기

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -36,9 +36,9 @@ export default function Home() {
             <header className="flex items-center justify-between h-14 px-6 fixed top-0 left-0 right-0 bg-white border-b border-mint-100 z-50">
                 <div className="w-24"></div>
 
-                <span className="text-2xl font-semibold font-en text-mint-900 text-center">
+                <Link to='/' className="text-2xl font-semibold font-en text-mint-900 text-center">
                     EZREAD
-                </span>
+                </Link>
                 
                 <nav className="flex items-center justify-end text-sm text-mint-600">
                     { userInfo ? (
@@ -68,11 +68,8 @@ export default function Home() {
                 </nav>
             </header>
 
-            <div className="h-14" />
-
-            {/* 페이지 콘텐츠 */}
             <main className="w-full p-12">
-                {/* 여기에 콘텐츠 */}
+                
             </main>
 
         </div>
--- src/components/MyPage.tsx
+++ src/components/MyPage.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
-import type { UsageLogResponse } from "../api/types";
+import type { UsageLogResponse, UserInfoResponse } from "../api/types";
 import * as log from '../api/usageLog.ts';
+import * as auth from '../api/auth.ts';
+import { Link, useNavigate } from "react-router-dom";
 
 export default function MyPage() {
 
     const [usageLog, setUsageLog] = useState<UsageLogResponse>();
+    const [userInfo, setUserInfo] = useState<UserInfoResponse>();
     const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string | null>(null);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchUsageLog = async() => {
@@ -21,6 +25,18 @@ export default function MyPage() {
                 setIsLoading(false);
             }
         }
+        const fetchUserInfo = async() => {
+                    try {
+                        setIsLoading(true);
+                        const res = await auth.getUserInfo();
+                        setUserInfo(res);
+                    } catch(err) {
+                        console.log(err.response.detail.data);
+                    } finally {
+                        setIsLoading(false);
+                    }
+                }
+        fetchUserInfo();
         fetchUsageLog();
     },[]);
 
@@ -32,14 +48,6 @@ export default function MyPage() {
         );
     }
 
-    if (error) {
-        return (
-            <div className="bg-mint-50 w-full min-h-screen flex flex-col items-center justify-center font-ko">
-                <span className="text-red-500">오류: {error}</span>
-            </div>
-        );
-    }
-
     if (!usageLog) {
         return (
             <div className="bg-mint-50 w-full min-h-screen flex flex-col items-center justify-center font-ko">
@@ -47,11 +55,51 @@ export default function MyPage() {
             </div>
         );
     }
+    
+    const handleLogout = () => {
+        localStorage.removeItem('accessToken');
+        setUserInfo(undefined);
+        navigate('/');
+    }
 
     return(
         <div className="bg-mint-50 w-full min-h-screen flex flex-col items-center font-ko">
+            <header className="flex items-center justify-between h-14 px-6 fixed top-0 left-0 right-0 bg-white border-b border-mint-100 z-50">
+                <div className="w-24"></div>
+
+                <Link to='/' className="text-2xl font-semibold font-en text-mint-900 text-center">
+                    EZREAD
+                </Link>
+                
+                <nav className="flex items-center justify-end text-sm text-mint-600">
+                    { userInfo ? (
+                        <div className="flex gap-10 pr-36">
+                        <span>
+                            안녕하세요, <span className="text-mint-900 font-bold">{userInfo?.username}</span>님
+                        </span>
+                        <Link to='/mypage' className="hover:text-mint-900 transition-colors">
+                            마이페이지
+                        </Link>
+                        <button onClick={handleLogout} className="hover:text-mint-900 transition-colors">
+                            로그아웃
+                        </button>
+                        </div>
+                    ) : (
+                        <div className="flex gap-10 pr-36">
+                            <Link to="/login" className="hover:text-mint-900 transition-colors">
+                                로그인
+                            </Link>
+                            <Link to="/signup" className="hover:text-mint-900 transition-colors">
+                                회원가입
+                            </Link>
+                        </div>
+                    )}
+                    
+                    
+                </nav>
+            </header>
             {Array.isArray(usageLog) ? (
-                <div>
+                <div className="pt-20">
                     {usageLog.map((item) => (
                         <div key={item.id}>
                             <span>{item.title}</span>
